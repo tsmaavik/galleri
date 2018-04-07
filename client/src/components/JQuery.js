8714c1b951ed9c8cbc54693b15fd649fc7d6a41d
Trond Smaavik <trond.smaavik@gmail.com>
2018-04-07 23:20:11 +0200

    Made bgndgallery work

--- client/src/components/JQuery.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import jQuery from 'jquery' // Import directly from node-modules
-window.jQuery = jQuery
-
-export default () => (jQuery)
---
+++ client/src/main.js
@@ -0,0 +1,49 @@
+function loadAlbumView(){
+    $("#content").load("./nanogallery.template.html");
+    $('#nanogallery2').nanogallery2('reload');
+}
+
+function loadSlideshowView() {
+    // $("#content").load("<script type='text/javascript'>loadBgndGallery();</script>");
+    loadBgndGallery();
+}
+
+
+var images = [];
+var gallery;
+const serverUrl = "http://localhost:5000";
+
+function loadBgndGallery(){
+    
+    this.getAllImages()
+        .then((res) => {
+            this.images = res.images;
+            this.loadGallery();
+        });
+}
+
+async function getAllImages()
+{        
+    const response = await fetch (serverUrl+'/api/albums/tur',);
+    const body = await response.json();
+    if(response.status !== 200) throw Error(body.message);
+    return body;  
+}
+
+function loadGallery(){                
+     gallery = new mbBgndGallery({
+        containment:"body",
+        timer:4000,
+        effTimer:2000,
+        controls:"#gallery_controls",
+        grayScale:false,
+        images: this.images,
+
+        onStart: function() {},
+        onPause: function() {},
+        onPlay: function( opt ) {},
+        onChange: function( opt, idx ) {},
+        onNext: function( opt ) {},
+        onPrev: function( opt ) {}
+    });    
+}
